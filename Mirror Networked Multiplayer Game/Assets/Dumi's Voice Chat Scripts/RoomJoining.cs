284189eb933ad09bee61f795dedc876fee2a5dd2
MaccccD <Dumisanimofolo@gmail.com>
2025-04-24 03:47:39 +0200

    Voice Chat Scene | Odin Manager Settings Update
    Yayyyyyy the voice chat Works!!!

--- Mirror Networked Multiplayer Game/Assets/Dumi's Voice Chat Scripts/RoomJoining.cs
+++ Mirror Networked Multiplayer Game/Assets/Dumi's Voice Chat Scripts/RoomJoining.cs
@@ -6,7 +6,7 @@ public class RoomJoining : MonoBehaviour
     void Start()
     {
         OdinHandler.Instance.JoinRoom("TheLitZone"); //Dumi: this handler is a singleton( a design pattern that ensures that a class only has one instance throughout). It will assign this script to an empty game object and ensures that it does not destroy on load to keep the singleton alove even if the scene changes.
-
+         Debug.Log("Attempting to join room: TheLitZone");
         //Dumi: In Odin, a rooom is a virtual spave where peers(users in the room) can communicate with each other.
     }
 
